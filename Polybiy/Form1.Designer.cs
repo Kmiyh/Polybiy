acac7fc121329b3a27643c567a61e30545016540
Pavel Galiguzov <pvlgaliguzov@outlook.com>
2021-03-04 18:57:43 +0300

    Добавьте файлы проекта.

---
+++ Polybiy/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Polybiy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.a1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(451, 89);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.a1,
+            this.a2,
+            this.a3,
+            this.a4,
+            this.a5,
+            this.a6});
+            this.dataGridView1.Location = new System.Drawing.Point(102, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(269, 134);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // a1
+            // 
+            this.a1.HeaderText = "1";
+            this.a1.Name = "a1";
+            this.a1.Width = 38;
+            // 
+            // a2
+            // 
+            this.a2.HeaderText = "2";
+            this.a2.Name = "a2";
+            this.a2.Width = 38;
+            // 
+            // a3
+            // 
+            this.a3.HeaderText = "3";
+            this.a3.Name = "a3";
+            this.a3.Width = 38;
+            // 
+            // a4
+            // 
+            this.a4.HeaderText = "4";
+            this.a4.Name = "a4";
+            this.a4.Width = 38;
+            // 
+            // a5
+            // 
+            this.a5.HeaderText = "5";
+            this.a5.Name = "a5";
+            this.a5.Width = 38;
+            // 
+            // a6
+            // 
+            this.a6.HeaderText = "6";
+            this.a6.Name = "a6";
+            this.a6.Width = 38;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Шифровка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Расшифровка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 301);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(451, 89);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 451);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Шифр Полибия";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Polybiy/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Polybiy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string abc = "abcdefghijklmnopqrstuvwxyz .,?";
+            int count = 0;
+            dataGridView1.Rows.Add(5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+
+                for (int j = 0; j < 6; j++)
+                {
+                    dataGridView1[j, i].Value = abc[count];
+                    count++;
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string CryptText = "";
+
+            for (int x = 0; x < text.Length; x++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (text[x] == Convert.ToChar(dataGridView1[j, i].Value))
+                        {
+                            if (i + 1 > 4)
+                            {
+                                CryptText += dataGridView1[j, 0].Value;
+                            } else
+                            {
+                                CryptText += dataGridView1[j, i + 1].Value;
+                            }
+                        }
+
+                    }
+                }
+            }
+            textBox2.Text = CryptText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string EncryptText = "";
+
+            for (int x = 0; x < text.Length; x++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (text[x] == Convert.ToChar(dataGridView1[j, i].Value))
+                        {
+                            if (i - 1 < 0)
+                            {
+                                EncryptText += dataGridView1[j, 4].Value;
+                            }
+                            else
+                            {
+                                EncryptText += dataGridView1[j, i - 1].Value;
+                            }
+                        }
+
+                    }
+                }
+            }
+            textBox2.Text = EncryptText;
+        }
+    }
+}
