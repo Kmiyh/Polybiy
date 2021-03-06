7111c89bb8c9b4a1c3c8b41fba5811c2ab6beffc
Pavel Galiguzov <pvlgaliguzov@outlook.com>
2021-03-06 16:42:01 +0300

    Вторая матрица Пофиксить проблему с индексами

--- Permutation/Form1.Designer.cs
+++ Permutation/Form1.Designer.cs
@@ -38,29 +38,37 @@ namespace Permutation
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(66, 6);
+            this.textBox1.Location = new System.Drawing.Point(676, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "5";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(66, 33);
+            this.textBox2.Location = new System.Drawing.Point(676, 57);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "7";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(367, 82);
+            this.button1.Location = new System.Drawing.Point(607, 128);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 39);
+            this.button1.Size = new System.Drawing.Size(195, 43);
             this.button1.TabIndex = 2;
             this.button1.Text = "Зашифровать";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,15 +77,15 @@ namespace Permutation
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 127);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 177);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(451, 199);
+            this.dataGridView1.Size = new System.Drawing.Size(691, 199);
             this.dataGridView1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Location = new System.Drawing.Point(619, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 4;
@@ -86,7 +94,7 @@ namespace Permutation
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 36);
+            this.label2.Location = new System.Drawing.Point(619, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 5;
@@ -94,18 +102,18 @@ namespace Permutation
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(305, 6);
+            this.textBox3.Location = new System.Drawing.Point(12, 128);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(158, 43);
+            this.textBox3.Size = new System.Drawing.Size(589, 43);
             this.textBox3.TabIndex = 6;
             this.textBox3.Text = "неясное становится еще более непонятным";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(234, 9);
+            this.label3.Location = new System.Drawing.Point(12, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 13);
             this.label3.TabIndex = 7;
@@ -114,25 +122,73 @@ namespace Permutation
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(234, 59);
+            this.label4.Location = new System.Drawing.Point(620, 87);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(33, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Ключ";
             // 
-            // textBox4
+            // dataGridView2
             // 
-            this.textBox4.Location = new System.Drawing.Point(305, 56);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(158, 20);
-            this.textBox4.TabIndex = 9;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(709, 177);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(659, 199);
+            this.dataGridView2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 379);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Исходная матрица";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1228, 379);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Результирующая матрица";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(808, 128);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(560, 43);
+            this.textBox5.TabIndex = 13;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(676, 83);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1569325056,
+            23283064,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4751623,
+            0,
+            0,
+            0});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 446);
-            this.Controls.Add(this.textBox4);
+            this.ClientSize = new System.Drawing.Size(1380, 625);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
@@ -145,6 +201,8 @@ namespace Permutation
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,7 +219,11 @@ namespace Permutation
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- Permutation/Form1.cs
+++ Permutation/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Permutation
@@ -20,13 +13,16 @@ namespace Permutation
         private void button1_Click(object sender, EventArgs e)
         {
             string replacedText = textBox3.Text.Replace(" ", "");
+            string replacedKey = numericUpDown1.Value.ToString();
             int count = 0;
+            int newCount = 0;
+            string temp = "";
             int rows = Convert.ToInt32(textBox1.Text);
             int columns = Convert.ToInt32(textBox2.Text);
 
             for (int j = 0; j < columns; j++)
             {
-                dataGridView1.Columns.Add((j+1).ToString(), (j+1).ToString());
+                dataGridView1.Columns.Add(j.ToString(), j.ToString());
             }
 
             for (int i = 0; i < rows - 1; i++)
@@ -36,10 +32,39 @@ namespace Permutation
 
             for (int j = 0; j < columns; j++)
             {
+                dataGridView2.Columns.Add(j.ToString(), j.ToString());
+            }
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                dataGridView2.Rows.Add();
+            }
+
+            for (int j = 0; j < columns; j++)
+            {
+
                 for (int i = 0; i < rows; i++)
                 {
                     dataGridView1[j, i].Value = replacedText[count];
                     count++;
+                }
+            }
+
+            for (int ch = 0; ch < replacedKey.Length; ch++)
+            {
+                for (int col = 0; col < columns; col++) 
+                {
+                    int colIndex = dataGridView1.Columns[col].Index;
+                    if (colIndex.ToString() == (replacedKey[ch]).ToString())
+                    {
+                        for (int i = 0; i < rows; i++)
+                        {
+                            temp += dataGridView1[col, i].Value.ToString();
+                            dataGridView2[ch, i].Value = temp[newCount];
+                            newCount++;
+                        }
+                        break;
+                    }
                 }
             }
         }
