43895abca34a00c377c3f4a3a376b62c11beecf7
Pavel Galiguzov <pvlgaliguzov@outlook.com>
2021-03-07 16:25:22 +0300

    один нклевой цикл

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 5;
+            string[] buf = new string[SIZE] {"guhoe",
+                      "llxoe",
+                      "bnce.",
+                      "utp!w",
+                      "nqwmp"};
+            int[,] grid = new int[SIZE, SIZE]{{0, 0, 1, 0, 1},
+                          {1, 1, 0, 1, 0},
+                          {0, 0, 0, 0, 0},
+                          {0, 0, 0, 1, 0},
+                          {0, 0, 0, 0, 0}};
+            // вывод зашифрованного сообщения
+            for (int i = 0; i < SIZE; i++)
+            {
+                Console.WriteLine(buf[i]);
+            }
+            Console.WriteLine("");
+
+            // прямой обход решетки
+            Console.WriteLine("0:");
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++)
+                    if (grid[i, j] == 1)
+                    {
+                        Console.Write(buf[i][j]);
+                    }
+            Console.WriteLine("");
+            // поворот решетки на 90 градусов по часовой стрелке
+            Console.WriteLine("90:");
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++)
+                    if (grid[SIZE - j - 1, i] == 1)
+                    {
+                        Console.Write(buf[i][j]);
+                    }
+            Console.WriteLine("");
+            // поворот решетки на 180 градусов по часовой стрелке
+            Console.WriteLine("180:");
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++)
+                    if (grid[SIZE - i - 1, SIZE - j - 1] == 1)
+                    {
+                        Console.Write(buf[i][j]);
+                    }
+            Console.WriteLine("");
+            // поворот решетки на 270 градусов по часовой стрелке
+            Console.WriteLine("270:");
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++)
+                    if (grid[j, SIZE - i - 1] == 1)
+                    {
+                        Console.Write(buf[i][j]);
+                    }
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ Stencil/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stencil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(textBox1.Text);
+            // создать размерность трафарета
+            for (int j = 0; j < size; j++)
+            {
+                dataGridView1.Columns.Add(j.ToString(), "");
+            }
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+
+            for (int j = 0; j < size; j++)
+            {
+                dataGridView2.Columns.Add(j.ToString(), "");
+            }
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                dataGridView2.Rows.Add();
+            }
+
+            // заполняем нулями
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    dataGridView1[i, j].Value = 0;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox3.Text;
+            int count = 0;
+            int size = Convert.ToInt32(textBox1.Text);
+            int[,] grid = new int[size, size];
+            int[,] resultGrid = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    grid[j, i] = Convert.ToInt32(dataGridView1[j, i].Value);
+                }
+            }
+
+            text = text.Remove(size, size*(size-1));
+            MessageBox.Show(text);
+                for (int j = 0; j < size; j++)
+                {
+                    for (int k = 0; k < size; k++)
+                    {
+                        if (dataGridView1[k, j].Value.ToString() == 1.ToString())
+                        {
+                            //MessageBox.Show(text[count].ToString());
+                            dataGridView2[k, j].Value = text[count];
+                            
+                            count++;
+                        }
+                }
+            }
+        }
+    }
+}
